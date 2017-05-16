658848abc04d07d348f5f82477d4aa5bd3f59295
Daniel Owusu Mensah <Daniel Owusu Mensah>
2017-05-16 22:05:16 +0100

    changed http header for production

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -1,11 +1,16 @@
 import { Promise } from 'es6-promise'
 import $ from 'jquery'
 
+function generateUrl(url) {
+  const isProd = process.env.NODE_ENV === "production";
+  return `${isProd ? 'https' : 'http'}://api.olep.co.uk${url}`;
+}
+
 export default function(url, type, options = {}) {
   return new Promise((resolved, rejected) => {
     $.ajax({
       type: type,
-      url: `http://api.olep.co.uk${url}`,
+      url: generateUrl(url),
       data: options,
       success: (data) => resolved(JSON.parse(data)),
       error: (data) => rejected(data)
